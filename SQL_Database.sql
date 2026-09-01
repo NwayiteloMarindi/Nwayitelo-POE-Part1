6b12557f1cd5b9da4644638dba9a8995c7c37adc
NwayiteloMarindi <ST10472536@rcconnect.edu.za>
2026-09-01 10:10:18 +0200

    Create RaceDayDB and VENUE table with constraints

--- SQL_Database.sql
+++ SQL_Database.sql
@@ -1,2 +1,15 @@
 
 CREATE DATABASE RaceDayDB;
+
+
+CREATE TABLE VENUE (
+    venue_id INT IDENTITY(1,1) PRIMARY KEY,
+    venue_name VARCHAR(100) NOT NULL,
+    location VARCHAR(150) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    province VARCHAR(100) NOT NULL,
+    capacity INT NOT NULL,
+    
+    CONSTRAINT CK_Venue_Capacity
+        CHECK (capacity > 0)
+);
